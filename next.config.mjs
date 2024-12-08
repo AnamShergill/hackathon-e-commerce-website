9af3257cf097564301264b64192b2f21ac06639a
Anam Shergill <pinkyshergill1986@gmail.com>
2024-12-09 01:24:41 +0500

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/old-url',
+          destination: '/new-url',
+          permanent: true,
+        },
+      ];
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/custom-route',
+          destination: '/new-page', // internally maps to /new-page
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
